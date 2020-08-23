215deaa91ebe2c7b34c99d0fb835b7ee0f9442c3
Christian Santos <christian.pinho@outlook.com>
2020-08-23 03:04:42 -0300

    add map and marking on.

---
+++ src/shared/services/Api.ts
@@ -0,0 +1,14 @@
+const APY_KEY = '37f3996f75583a82006b7c3d0443802c';
+
+interface latlng {
+  lat: number;
+  lng: number;
+}
+
+const getWeather = ({ lat, lng }: latlng) => {
+  return fetch(
+    `http://api.openweathermap.org/data/2.5/find?lat=${lat}&lon=${lng}&cnt=15&APPID=${APY_KEY}`,
+  ).then((response) => response.json());
+};
+
+export { getWeather };
